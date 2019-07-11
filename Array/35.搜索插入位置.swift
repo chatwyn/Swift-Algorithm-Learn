6125c40d020b00df66fd814b0cc006f05359a9f4
caowenbo <caowenbo@gotokeep.com>
2019-07-11 23:41:08 +0800

    算法初体验

---
+++ Array/35.搜索插入位置.swift
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=swift
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (43.73%)
+ * Likes:    269
+ * Dislikes: 0
+ * Total Accepted:    54.5K
+ * Total Submissions: 124.5K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 
+ * 你可以假设数组中无重复元素。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ * 
+ * 
+ */
+class Solution {
+      func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var index = 0
+        for i in 0 ..< nums.count {
+            if nums[i] < target {
+                index += 1
+            } else {
+                break
+            }
+        }
+        return index
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Swift-Algorithm-Learn
+
+记录一下刷算法的过程
